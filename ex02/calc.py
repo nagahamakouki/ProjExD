c7337f0214fe72a0318c6f322a566268694f8e7e
nagahama <c0a2115383@edu.teu.ac.jp>
2022-11-29 14:23:31 +0900

    第２問

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,4 +2,14 @@ import tkinter as tk
 
 root = tk.Tk()
 root.geometry("300x500")
+
+r,c=0,0
+for i in range(10):
+   button =tk.Button(root,text=f"{i}",width=4,height=2,font=("",30))
+   button.grid(row =r,column = c)
+   c+=1
+   if c%3 ==0:
+    r+=1
+    c=0
+
 root.mainloop()
