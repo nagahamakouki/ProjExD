6fc2e497b8c96e9254bcedac127c3123e92f7efa
nagahama <c0a2115383@edu.teu.ac.jp>
2022-11-29 15:49:53 +0900

    四則演算の導入

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x650")
 
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row=0, column=0, columnspan=3)
@@ -14,14 +14,18 @@ def button_click(event):
         siki = entry.get()
         res = eval(siki)
         entry.delete(0,tk.END)
-        entry.insert(0,tk.END)
+        entry.insert(tk.END,res)
+    elif num == "AC":
+        entry.delete(0,tk.END) #ACを押すと表示内容の削除を行う
+
+
     else:
         entry.insert(tk.END,num)
     #tkm.showinfo("", f"{num}ボタンがクリックされました")
 
 r,c=1,2
 for i in range(9,-1,-1):
-   button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
+   button = tk.Button(root, text=f"{i}", width=4, height=1, font=("",30))
    button.grid(row=r, column=c)
    button.bind("<1>", button_click)
    c -= 1
@@ -29,10 +33,11 @@ for i in range(9,-1,-1):
     r += 1
     c = 2
 
-operators = ["+","="]
+operators = ["","=","+","-","/","*","A","AC"]
 for ope in operators:
-    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("",30))
+    button = tk.Button(root, text=f"{ope}", width=4, height=1, font=("",30))
     button.grid(row=r, column=c)
+    button.bind("<1>",button_click)
     c -= 1
     if c%3 == 2:
         r+=1
