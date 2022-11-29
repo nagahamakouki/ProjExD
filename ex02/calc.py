5b06a4c4d817add4972c9d7ba47023e5bf1fecc8
nagahama <c0a2115383@edu.teu.ac.jp>
2022-11-29 16:34:33 +0900

    ACとA

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x600")
 
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row=0, column=0, columnspan=3)
@@ -16,20 +16,25 @@ def button_click(event):
         entry.delete(0,tk.END)
         entry.insert(tk.END,res)
 
-    elif num == "AC":
+    elif num == "AC": #ALLCLEAR
         entry.delete(0,tk.END) #ACを押すと表示内容の全削除を行う
+
+    elif num =="A":
+        txt = entry.get()[:-1] #最後の一文字を抜いた文字式の取得
+        entry.delete(0,tk.END) #全部消す
+        entry.insert(0,txt) #最後の一文字を抜いた文字式の挿入
 
     elif num == "x^2":
         siki = entry.get() #文字列の取得
         res = eval(siki) #評価
         ni = res*res #二乗の計算
-        entry.delete(0,tk.END)
-        entry.insert(tk.END,ni)
+        entry.delete(0,tk.END) 
+        entry.insert(tk.END,ni) #結果の挿入
 
     elif num == "%":
         siki = entry.get()
-        if siki == int:
-            res = int(siki)/100
+        if siki == int: #整数か小数かの判別
+            res = int(siki)/100 #百分率にする
         else:
             res = float(siki)/100
         entry.delete(0,tk.END)
@@ -49,7 +54,7 @@ for i in range(9,-1,-1):
     r += 1
     c = 2
 
-operators = ["=","AC","+","-","/","*","x^2","%"]
+operators = ["=","AC","+","-","/","*","x^2","%","A"]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=1, font=("",30))
     button.grid(row=r, column=c)
