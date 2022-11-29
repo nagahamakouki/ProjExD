bee0ed26708c09309d81baba4e694525500a7ee8
nagahama <c0a2115383@edu.teu.ac.jp>
2022-11-29 14:43:53 +0900

    第5問

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,22 +4,31 @@ import tkinter.messagebox as tkm
 root = tk.Tk()
 root.geometry("300x500")
 
-entry = tk.Entry(root,justify = "right",width = 10,font = ("",40))
-entry.grid(row = 0,column = 0,columnspan = 3)
+entry = tk.Entry(root, justify="right", width=10, font=("",40))
+entry.grid(row=0, column=0, columnspan=3)
 
 def button_click(event):
     btn = event.widget
     num = btn["text"]
-    tkm.showinfo("",f"{num}ボタンがクリックされました")
+    tkm.showinfo("", f"{num}ボタンがクリックされました")
 
 r,c=1,0
 for i in range(10):
-   button = tk.Button(root,text = f"{i}",width = 4,height = 2,font = ("",30))
-   button.grid(row = r,column = c)
-   button.bind("<1>",button_click)
+   button = tk.Button(root, text=f"{i}", width=4, height=2, font=("",30))
+   button.grid(row=r, column=c)
+   button.bind("<1>", button_click)
    c += 1
    if c%3 == 0:
     r += 1
     c = 0
+
+operators = ["+","="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("",30))
+    button.grid(row=r, column=c)
+    c += 1
+    if c%3 ==0:
+        r+=1
+        c = 0
 
 root.mainloop()
